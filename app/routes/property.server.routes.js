ad85ca885413d4db57c1e43c96726ed504f36313
Andela Dev <olaide.agboola@andela.co>
2015-06-03 17:37:42 +0100

    added multer module to handle file upload at backend

--- app/routes/property.server.routes.js
+++ app/routes/property.server.routes.js
@@ -1,11 +1,25 @@
 'use strict';
 var property = require('./../controllers/property.server.controller');
+var multer = require('multer');
+var done=false;
 
 module.exports = function(app) {
+  app.use(multer({ dest: './uploads/',
+    rename: function (fieldname, filename) {
+      return filename + Date.now();
+    },
+    onFileUploadStart: function (file) {
+      console.log(file.originalname + ' is starting ...');
+    },
+    onFileUploadComplete: function (file) {
+      console.log(file.fieldname + ' uploaded to  ' + file.path);
+      done = true;
+    }
+  }));
 
   app.route('/properties')
     .get(property.list)
-    .post(property.create);
+    .post(property.uploadImage, property.create);
 
   app.route('/properties/:propertyId')
     .get(property.read)

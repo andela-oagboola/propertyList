ebad876f785b343fe070a9ab9a98884de89be1d6
Andela Dev <olaide.agboola@andela.co>
2015-06-04 11:16:19 +0100

    included property author in view

--- public/modules/properties/controllers/viewProperty.client.controller.js
+++ public/modules/properties/controllers/viewProperty.client.controller.js
@@ -1,8 +1,6 @@
 'use strict';
 angular.module('properties').controller('ViewPropertyCtrl', ['$scope', '$location', '$stateParams', 'backendService', function($scope, $location, $stateParams, backendService) {
-  console.log($stateParams);
   backendService.getSingleProperty($stateParams.propertyId).success(function (property) {
-    console.log(property);
     $scope.property = property[0];
   });
 }]);

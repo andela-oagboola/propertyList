3b9828a96801bc9b80bcad859668d6da3652fb2b
Andela Dev <olaide.agboola@andela.co>
2015-06-04 09:41:19 +0100

    added apii call to get single property

--- public/modules/properties/services/properties.client.service.js
+++ public/modules/properties/services/properties.client.service.js
@@ -10,5 +10,8 @@ angular.module('properties').factory('backendService', ['$http', function ($http
       return $http.post('/properties', property);
     },
 
+    getSingleProperty: function (propertyId) {
+      return $http.get('/properties/' + propertyId);
+    }
   };
 }]);

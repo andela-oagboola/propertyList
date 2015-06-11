893d68847af57b6849d3362861603815895bd0d4
Andela Dev <olaide.agboola@andela.co>
2015-06-11 16:00:03 +0100

    authenticating routes

--- app/routes/property.server.routes.js
+++ app/routes/property.server.routes.js
@@ -1,5 +1,6 @@
 'use strict';
 var property = require('./../controllers/property.server.controller');
+var users = require('../../app/controllers/users.server.controller');
 var multer = require('multer');
 var done=false;
 
@@ -19,15 +20,15 @@ module.exports = function(app) {
 
   app.route('/properties')
     .get(property.list)
-    .post(property.uploadImage, property.create);
+    .post(users.requiresLogin, property.uploadImage, property.create);
 
   app.route('/properties/:propertyId')
     .get(property.read)
-    .put(property.uploadImage, property.update)
-    .delete(property.delete);
+    .put(users.requiresLogin, property.uploadImage, property.update)
+    .delete(users.requiresLogin, property.delete);
 
-  app.route('/properties/user/:userId').get(property.getUserProperty);
+  app.route('/properties/user/:userId').get(users.requiresLogin, property.getUserProperty);
 
-  app.route('/properties/:propertyId/contactAgent').post(property.mailProperyOwner);
+  app.route('/properties/:propertyId/contactAgent').post(users.requiresLogin, property.mailProperyOwner);
   
 };

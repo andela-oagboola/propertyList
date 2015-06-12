bb051da47156878ae0cb317b2ae3ca94170b45c3
Andela Dev <olaide.agboola@andela.co>
2015-06-12 12:15:02 +0100

    prevented logged in user from accessing user properties

--- public/modules/properties/controllers/userProperties.client.controller.js
+++ public/modules/properties/controllers/userProperties.client.controller.js
@@ -1,6 +1,10 @@
 'use strict';
-angular.module('properties').controller('UserProperties', ['Authentication', '$scope', 'backendService', function(Authentication, $scope, backendService){
+angular.module('properties').controller('UserProperties', ['Authentication', '$scope', 'backendService', '$location', function(Authentication, $scope, backendService, $location){
   $scope.user = Authentication.user;
+  if (!$scope.user) {
+    alert('you have to be logged in');
+    $location.path('/');
+  }
   backendService.getUserProperties($scope.user._id).success(function (response) {
     console.log(response);
     $scope.properties = response;
